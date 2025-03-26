a2e3fe9fdd0d9d5ca30a2d0e7ffdec5471c14559
moore0601 <muhammedoyebanji@gmail.co>
2025-03-26 15:30:44 +0000

    update

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("my people");
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<numberOfDrumButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert("I got clicked!");
+    });
+
+}
